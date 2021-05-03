4774a1b526b8e42107d8bfc4e958765739dae387
atap9000 <atap9000@gmail.com>
2021-05-03 00:50:08 -0400

    Segundo commit: Inicio del login al sistema

---
+++ app/controllers/principal_controller.rb
@@ -0,0 +1,6 @@
+class PrincipalController < ApplicationController
+before_action :authenticate_user!
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'principal#index' #Ruta de la pagina principal
+
+  get '/users/sign_in', to: redirect("/login") # Redireccion hacia pagina de login NO BORRAR
+
+  devise_scope :user do
+    get "/login" => "devise/sessions#new", as: "new_session_user"  #Pagina de Login
+  end
+
+  devise_for :users
+  get '*path' => redirect('/') #en caso de que la ruta no existe, redirecciona al root
 end
