f7e91faf1f7185d65ffcab5c678afa294a793da1
atap9000 <atap9000@gmail.com>
2021-05-13 00:54:14 -0400

    Unas seeds añadidas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Rol.create(nombre: "admin")
+Rol.create(nombre: "gerente")
+Rol.create(nombre: "rrhh")
+Rol.create(nombre: "vendedor")
+Rol.create(nombre: "almacenista")
+User.create(email: "admin@admin", username: "administrador", password: "000000", password_confirmation: "000000", rol_id: "1")
+User.create(email: "notadmin@notadmin", username: "noadministrador", password: "111111", password_confirmation: "111111", rol_id: "2")
